40559fb7c407329db5c356af2268e4b593df17e6
Chris Risner <github@chrisrisner.com>
2013-02-19 08:28:22 -0800

    update

--- source/TicTacToe-LeaderBoard/src/com/msdpe/tictactoe_leaderboard/ScoreboardFragment.java
+++ source/TicTacToe-LeaderBoard/src/com/msdpe/tictactoe_leaderboard/ScoreboardFragment.java
@@ -36,9 +36,9 @@ public class ScoreboardFragment extends Fragment {
 		View headerView = this.getActivity().getLayoutInflater().inflate(R.layout.leaderboard_header, null);
 		lvLeaderboard.addHeaderView(headerView);
 		
-		String[] names = new String[] { "Linux", "Windows7", "Eclipse", "Suse",
-		        "Ubuntu", "Solaris", "Android", "iPhone", "Linux", "Windows7",
-		        "Eclipse", "Suse", "Ubuntu", "Solaris", "Android", "iPhone" };
+		String[] names = new String[] { "Player One", "Player 2", "Player 3", "Player 4",
+		        "Player 5", "Player 6", "Player 7", "Player 8", "Player 9", "Player 10",
+		        "Player 11", "Player 12", "Player 13", "Player 14", "Player 15", "Player 16" };
 		
 		lvLeaderboard.setAdapter(new ArrayAdapter<String>(this.getActivity(),
 		        android.R.layout.simple_list_item_single_choice,
